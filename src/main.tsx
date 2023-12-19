a358d9a068ad120e33935a132506ac31da46edfa
Maston <22679886+mastondzn@users.noreply.github.com>
2023-12-19 06:57:03 +0200

    properly check html for root id

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,10 @@ import { createRoot } from 'react-dom/client';
 import App from './app.tsx';
 import { ThemeProvider } from './components/theme-provider.tsx';
 
-createRoot(document.querySelector('#root')!).render(
+const root = document.querySelector('#root');
+if (!root) throw new Error('Root element not found');
+
+createRoot(root).render(
     <StrictMode>
         <ThemeProvider>
             <App />
