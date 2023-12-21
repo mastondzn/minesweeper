b9d83c1dfee7480fa3140049634c5dc9605a8637
Maston <22679886+mastondzn@users.noreply.github.com>
2023-12-21 08:55:25 +0200

    improve performance, explicitly use immer

--- src/utils/minesweeper.ts
+++ src/utils/minesweeper.ts
@@ -1,50 +1,49 @@
+import { current, type Draft, produce } from 'immer';
 import { create } from 'zustand';
-import { immer } from 'zustand/middleware/immer';
 
 import { createGrid, determineWinCondition, updateNeighbors } from './helpers';
 import { type PresetName, presets } from './presets';
 import { storage } from './storage';
-import { type Cell, type Coordinates, type Minesweeper, type MinesweeperState } from './types';
+import { type Cell, type Minesweeper, type MinesweeperState } from './types';
 
 const settings = storage.get('settings');
 
+const resetGame = (draft: Draft<MinesweeperState>) => {
+    draft.grid = createGrid(presets[draft.settings.preset]);
+    draft.gameStatus = 'playing';
+    draft.startedAt = null;
+    draft.endedAt = null;
+};
+
 // ts server donking up here for some reason
-export const useMinesweeper = create<Minesweeper>()(
-    immer((set) => ({
-        settings,
-        grid: createGrid(presets[settings.preset]),
-        gameStatus: 'playing',
-        startedAt: null,
-        endedAt: null,
-        choosePreset: (preset: PresetName) => {
-            set((state: MinesweeperState) => {
-                storage.set('settings', { preset });
-                state.settings.preset = preset;
-                state.grid = createGrid(presets[state.settings.preset]);
-                state.gameStatus = 'playing';
-                state.startedAt = null;
-                state.endedAt = null;
-            });
-        },
-        reset: () => {
-            set((state: MinesweeperState) => {
-                state.grid = createGrid(presets[state.settings.preset]);
-                state.gameStatus = 'playing';
-                state.startedAt = null;
-                state.endedAt = null;
-            });
-        },
-        click: ({ x, y }) => {
-            set((state: MinesweeperState) => {
-                if (state.gameStatus !== 'playing') return;
+export const useMinesweeper = create<Minesweeper>()((set) => ({
+    settings,
+    grid: createGrid(presets[settings.preset]),
+    gameStatus: 'playing',
+    startedAt: null,
+    endedAt: null,
+    reset: () => set((state: MinesweeperState) => produce(state, resetGame)),
+    choosePreset: (preset: PresetName) => {
+        set((state: MinesweeperState) => {
+            storage.set('settings', { preset });
 
-                let cell = state.grid[y]?.[x];
-                if (!cell) throw new Error('Cell does not exist');
-                if (cell.flagged) return; // must not be flagged to be clicked
+            return produce(state, (draft) => {
+                draft.settings.preset = preset;
+                resetGame(draft);
+            });
+        });
+    },
+    click: ({ x, y }) => {
+        set((state: MinesweeperState) => {
+            return produce(state, (draft) => {
+                if (draft.gameStatus !== 'playing') return;
 
-                if (!state.startedAt) {
-                    state.startedAt = new Date();
-                    const directive = state.settings.startDirective;
+                let cell = draft.grid[y]?.[x];
+                if (!cell || cell.flagged) return;
+
+                if (!draft.startedAt) {
+                    draft.startedAt = new Date();
+                    const directive = draft.settings.startDirective;
 
                     const shouldRegenerate = (cell: Cell) => {
                         return directive === 'empty'
@@ -56,67 +55,57 @@ export const useMinesweeper = create<Minesweeper>()(
 
                     let regenerate = shouldRegenerate(cell);
                     while (regenerate) {
-                        state.grid = createGrid(presets[state.settings.preset]);
+                        draft.grid = createGrid(presets[draft.settings.preset]);
                         console.log('regenerating grid');
-                        cell = state.grid[y]?.[x];
-                        if (!cell) throw new Error('Cell does not exist');
+                        cell = draft.grid[y]![x]!;
                         regenerate = shouldRegenerate(cell);
                     }
                 }
 
                 cell.visible = true;
                 if (cell.type === 'mine') {
-                    // @ts-expect-error ts server cant realize both these are ok
-                    state.gameStatus = 'lost';
-                    // @ts-expect-error ts server cant realize both these are ok
-                    state.endedAt = new Date();
+                    // we cannot assign to draft and spread to please TS (see below)
+                    // https://immerjs.github.io/immer/pitfalls#dont-reassign-the-recipe-argument
+                    // @ts-expect-error see above
+                    draft.gameStatus = 'lost';
+                    // @ts-expect-error see above
+                    draft.endedAt = new Date();
                     return;
                 } else if (cell.type === 'empty') {
-                    state.grid = updateNeighbors(state.grid, { x, y });
+                    draft.grid = updateNeighbors(
+                        // we should use current here since passing proxy object is slow, we assign grid here anyway
+                        current(draft).grid,
+                        { x, y },
+                    );
                 }
 
-                if (determineWinCondition(state.grid)) {
-                    // @ts-expect-error ts server cant realize both these are ok
-                    state.gameStatus = 'won';
-                    // @ts-expect-error ts server cant realize both these are ok
-                    state.endedAt = new Date();
+                if (determineWinCondition(draft.grid)) {
+                    // @ts-expect-error see above
+                    draft.gameStatus = 'won';
+                    // @ts-expect-error see above
+                    draft.endedAt = new Date();
                 }
             });
-        },
-        flag: ({ x, y }) => {
-            set((state: MinesweeperState) => {
-                if (state.gameStatus !== 'playing') return;
+        });
+    },
+    flag: ({ x, y }) => {
+        set((state: MinesweeperState) => {
+            return produce(state, (draft) => {
+                if (draft.gameStatus !== 'playing') return;
+
+                const cell = draft.grid[y]?.[x];
+
                 // TODO: we can make the game not be based on luck here in the future
-                const cell = state.grid[y]?.[x];
-                if (!cell) throw new Error('Cell does not exist');
-                if (cell.visible) return;
+                if (!cell || cell.visible) return;
                 cell.flagged = !cell.flagged;
 
-                if (determineWinCondition(state.grid)) {
+                if (determineWinCondition(draft.grid)) {
                     // @ts-expect-error ts server cant realize both these are ok
-                    state.gameStatus = 'won';
+                    draft.gameStatus = 'won';
                     // @ts-expect-error ts server cant realize both these are ok
-                    state.endedAt = new Date();
+                    draft.endedAt = new Date();
                 }
             });
-        },
-    })),
-);
-
-export const useMinesweeperCell = ({ x, y }: Coordinates) => {
-    return useMinesweeper((state) => {
-        const cell = state.grid[y]?.[x];
-        if (!cell) throw new Error('Cell does not exist');
-
-        return {
-            cell,
-            gameStatus: state.gameStatus,
-            click: () => {
-                state.click({ x, y });
-            },
-            flag: () => {
-                state.flag({ x, y });
-            },
-        };
-    });
-};
+        });
+    },
+}));
