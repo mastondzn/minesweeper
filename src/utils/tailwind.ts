a0314a363d238b28518f609ca4ceae2f2a6f3b9b
Maston <22679886+mastondzn@users.noreply.github.com>
2023-12-22 19:53:14 +0200

    use twc

--- src/utils/tailwind.ts
+++ src/utils/tailwind.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from 'clsx';
+import { createTwc } from 'react-twc';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const twx = createTwc({ compose: cn });
