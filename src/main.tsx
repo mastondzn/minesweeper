a2e58516bce5d0ce80c0af906737f9830d010c46
Maston <22679886+mastondzn@users.noreply.github.com>
2024-01-01 05:05:17 +0200

    add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import './index.css';
 
+import { BrowserTracing, init as initializeSentry, Replay } from '@sentry/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './app.tsx';
 import { ThemeProvider } from './components/theme-provider.tsx';
+
+initializeSentry({
+    dsn: 'https://cf4f01848d43efd5adbd6658da1165f9@o4506493464805376.ingest.sentry.io/4506493754277888',
+    integrations: [
+        new BrowserTracing({
+            tracePropagationTargets: ['localhost', 'minesweeper.maston.dev'],
+        }),
+        new Replay({
+            maskAllText: false,
+            blockAllMedia: false,
+        }),
+    ],
+    tracesSampleRate: 1,
+});
 
 const root = document.querySelector('#root');
 if (!root) throw new Error('Root element not found');
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), sentryVitePlugin({
+        org: "synopsisgg",
+        project: "minesweeper"
+    })],
+
     resolve: { alias: { '~': path.resolve(__dirname, './src') } },
+
+    build: {
+        sourcemap: true
+    }
 });
